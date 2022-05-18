20626be27f88a1174992ad28e9e144c15b53b104
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-05-18 13:23:26 +0300

    Day3 Task5 is done

---
+++ src/test/java/com/cydeo/tests/Day4_findElements_checkBoxes_radio/T1_xpath_cssSelector_practice.java
@@ -0,0 +1,4 @@
+package com.cydeo.tests.Day4_findElements_checkBoxes_radio;
+
+public class T1_xpath_cssSelector_practice {
+}
---
+++ src/test/java/com/cydeo/tests/Day4_findElements_checkBoxes_radio/T4_findElements.java
@@ -0,0 +1,62 @@
+package com.cydeo.tests.Day4_findElements_checkBoxes_radio;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class T4_findElements {
+    public static void main(String[] args) {
+
+        //TC #4: FindElements Task
+        //1- Open a chrome browser
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        // providing extra wait time for our driver before it throws NoSuchElement
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2- Go to: https://practice.cydeo.com/abtest
+        driver.get("https://practice.cydeo.com/abtest");
+
+        //3- Locate all the links in the page.
+        //windows : alt+enter
+        //max: option + enter
+        List<WebElement> allList = driver.findElements(By.tagName("a"));
+
+        //4- Print out the number of the links on the page.
+        System.out.println("allList.size() = " + allList.size());
+
+        //5- Print out the texts of the links.
+        //6- Print out the HREF attribute values of the links
+
+        for (WebElement each : allList) {
+            System.out.println("Text of link: "+each.getText());
+            System.out.println("href Value: "+each.getAttribute("href"));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
