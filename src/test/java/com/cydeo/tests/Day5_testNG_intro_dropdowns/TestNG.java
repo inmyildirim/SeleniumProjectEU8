cadd66b52db2f9a29b23da2a27f29321ac8865b1
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-05-19 13:10:17 +0300

    TestNG Intro done

---
+++ src/test/java/com/cydeo/tests/Day5_testNG_intro_dropdowns/TestNG.java
@@ -0,0 +1,51 @@
+package com.cydeo.tests.Day5_testNG_intro_dropdowns;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestNG {
+
+    @Test (priority = 1)
+    public void test1(){
+        System.out.println("Test 1 is running...");
+        String actual = "apple";
+        String expected = "apple2";
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test (priority = 2)
+    public void test2(){
+        System.out.println("Test 2 is running...");
+        //assert true
+        String actual = "apple";
+        String expected = "apple";
+
+        Assert.assertEquals(actual.equals(expected), "your message will go here");
+
+
+    }
+
+    @BeforeClass
+    public void setupMethod(){
+        System.out.println("-----> BeforeClass is running!");
+
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("-----> AfterClass is running!");
+
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("---> BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("---> AfterMethod is running!");
+    }
+
+
+}
