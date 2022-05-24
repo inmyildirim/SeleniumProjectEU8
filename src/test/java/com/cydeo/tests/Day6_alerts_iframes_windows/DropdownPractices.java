0c7dac2f1629916a2ad1837f7f308636d311c472
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-05-24 21:29:55 +0300

    Day6_ Task6

--- src/test/java/com/cydeo/tests/Day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/Day6_alerts_iframes_windows/DropdownPractices.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -12,25 +13,22 @@ import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
 
-    WebDriver driver;
-
+    public WebDriver driver;
 
     @BeforeMethod
     public void setupMethod(){
-        //1. Open Chrome browser
-        //2. Go to http://practice.cybertekschool.com/dropdown
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        //2. Go to https://practice.cydeo.com/dropdown
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://practice.cybertekschool.com/dropdown");
+        driver.get("https://practice.cydeo.com/dropdown");
     }
 
     @Test
     public void dropdown_task5() throws InterruptedException {
-        //TC #5: Selecting state from State dropdown and verifying result
+        //TC #5: Selecting state from State dropdown and verifying result 1. Open Chrome browse
 
-
-        // we located dropdown and it is ready to use
+        //we located the dropdown and it is ready to use
         Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
 
         //3. Select Illinois
@@ -49,30 +47,45 @@ public class DropdownPractices {
         String expectedOptionText = "California";
         String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
 
-        Assert.assertEquals(actualOptionText,expectedOptionText);
-
-        //Use all Select options. (visible text, value, index)
-
-
-
+        Assert.assertEquals(actualOptionText, expectedOptionText);
     }
+
     @Test
-    public void dropdown_Task6(){
-
-        //TC #6: Selecting date on dropdown and verifying
-
-        //3. Select “December 1st, 1923” and verify it is selected.
+    public void dropdown_task6(){
+        //Select “December 1 , 1923” and verify it is selected.
         Select yearDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
-        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
-        Select dayDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
+        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='month']")));
+        Select dayDropdown = new Select(driver.findElement(By.xpath("//select[@id='day']")));
 
         //Select year using : visible text
+        yearDropdown.selectByVisibleText("1923");
+
         //Select month using : value attribute
+        monthDropdown.selectByValue("11");
+
         //Select day using : index number
+        dayDropdown.selectByIndex(0);
 
+        //creating expected values
+        String expectedYear = "1923";
+        String expectedMonth = "December";
+        String expectedDay = "1";
 
+        //getting actual values from browser
+        String actualYear = yearDropdown.getFirstSelectedOption().getText();
+        String actualMonth = monthDropdown.getFirstSelectedOption().getText();
+        String actualDay = dayDropdown.getFirstSelectedOption().getText();
+
+        //create assertions
+        Assert.assertTrue(actualYear.equals(expectedYear));
+        Assert.assertEquals(actualMonth, expectedMonth);
+        Assert.assertEquals(actualDay, expectedDay);
 
     }
 
+    @AfterMethod
+    public void teardownMethod(){
+        driver.close();
+    }
 
 }
