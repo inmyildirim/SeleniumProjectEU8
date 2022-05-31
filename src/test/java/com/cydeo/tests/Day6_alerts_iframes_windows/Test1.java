69306f1dec2dc61957b432dfaa9fee623e597337
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-05-31 19:39:50 +0300

    Day9_ javaFaker

---
+++ src/test/java/com/cydeo/tests/Day6_alerts_iframes_windows/Test1.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.Day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebLauncher;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+        WebLauncher.webSiteLauncher("https://www.google.com");
+    }
+}
---
+++ src/test/java/com/cydeo/tests/webtables_utilities_javafaker/T1_WindowHandling.java
@@ -0,0 +1,69 @@
+package com.cydeo.tests.webtables_utilities_javafaker;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class T1_WindowHandling {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void window_handling_test(){
+        //2. Go to: https://www.amazon.com
+        driver.get("https://www.amazon.com");
+
+        //3. Copy-paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+
+        //4. Create a logic to switch to the tab where Etsy.com is open
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles) {
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+            if (driver.getCurrentUrl().contains("etys")){
+                break;
+            }
+        }
+
+        // 5. Assert: Title contains "Etsy"
+        String actualTitle =    driver.getTitle();
+        String expectedTitle = "Etys";
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
