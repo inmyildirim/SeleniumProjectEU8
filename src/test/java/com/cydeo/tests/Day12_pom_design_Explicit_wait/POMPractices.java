7b4e1b49b0525afd2e3181432b225c96939be581
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-06-11 16:03:32 +0300

    Day12 LibraryLoginPage.java

--- src/test/java/com/cydeo/tests/Day12_pom_design_Explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/Day12_pom_design_Explicit_wait/POMPractices.java
@@ -7,26 +7,83 @@ import org.testng.annotations.Test;
 
 public class POMPractices {
 
+    LibraryLoginPage libraryLoginPage;
     @Test
     public void required_field_error_message_text(){
         //TC #1: Required field error message test
         //1- Open a chrome browser
         //2- Go to: https://library1.cydeo.com
         Driver.getDriver().get("https://library1.cydeo.com");
-
-
         //3- Do not enter any information
         //4- Click to “Sign in” button
-        LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
+        libraryLoginPage = new LibraryLoginPage();
         libraryLoginPage.signInButton.click();
-
-
         //5- Verify expected error is displayed:
         Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
-
-
-
         //Expected: This field is required.
         //NOTE: FOLLOW POM DESIGN PATTERN
+        Driver.closeDriver();
     }
+
+    @Test
+    public void invalid_email_format_error_message(){
+        //TC #2: Invalid email format error message test
+        //1- Open a chrome browser
+        //2- Go to: https://library1.cydeo.com
+        Driver.getDriver().get("https://library1.cydeo.com");
+        //3- Enter invalid email format
+        libraryLoginPage = new LibraryLoginPage();
+        libraryLoginPage.inputUsername.sendKeys("somethingwrong");
+        libraryLoginPage.signInButton.click();
+
+        //4- Verify expected error is displayed:
+        Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
+        //Expected: Please enter a valid email address.
+        //NOTE: FOLLOW POM DESIGN PATTERN
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void library_negative_login_test(){
+        //TC #3: Library negative login
+        //1- Open a chrome browser
+        //2- Go to: https://library1.cydeo.com
+        Driver.getDriver().get("https://library1.cydeo.com");
+        libraryLoginPage = new LibraryLoginPage();
+        //3- Enter incorrect username or incorrect password
+        libraryLoginPage.inputUsername.sendKeys("wrong@username.com");
+        libraryLoginPage.inputPassword.sendKeys("wrongPassword");
+        libraryLoginPage.signInButton.click();
+
+        //4- Verify title expected error is displayed:
+        Assert.assertTrue(libraryLoginPage.wrongEmailorPasswordErrorMessage.isDisplayed());
+
+        //Expected: Sorry, Wrong Email or Password
+        //NOTE: FOLLOW POM DESIGN PATTERN
+        Driver.closeDriver();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
