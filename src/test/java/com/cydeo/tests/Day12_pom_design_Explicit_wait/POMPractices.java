da93cc3af2fbea354c85a6f14b0146d2c89e902d
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-06-11 15:28:55 +0300

    Day12 LibraryLoginPage.java

--- src/test/java/com/cydeo/tests/Day12_pom_design_Explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/Day12_pom_design_Explicit_wait/POMPractices.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.Day12_pom_design_Explicit_wait;
 
+import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
 import org.testng.annotations.Test;
 
@@ -11,9 +12,18 @@ public class POMPractices {
         //1- Open a chrome browser
         //2- Go to: https://library1.cydeo.com
         Driver.getDriver().get("https://library1.cydeo.com");
+
+
         //3- Do not enter any information
         //4- Click to “Sign in” button
+        LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
+        libraryLoginPage.signInButton.click();
+
+
         //5- Verify expected error is displayed:
+
+
+
         //Expected: This field is required.
         //NOTE: FOLLOW POM DESIGN PATTERN
     }
